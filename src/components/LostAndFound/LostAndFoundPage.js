92f750186ca405e1b4ff0c9ffe6b50ef08e926e1
LisaPenzareva <penzarevaea@gmail.com>
2021-07-28 13:02:55 +0200

    changes in lostandfoundpage

--- src/components/LostAndFound/LostAndFoundPage.js
+++ src/components/LostAndFound/LostAndFoundPage.js
@@ -10,36 +10,9 @@ const LostAndFoundPage = (props) => {
 
   return (
     <div>
-      <header className="homepage-header px-4 py-2">
-        <div className="blue-logo blue-logo-header">
-          <NavLink to="/"> </NavLink>
-        </div>
-      </header>
-
       <section className="container-fluid">
-
-        <div className="row align-items-stretch ">
-          <div className="sidebar-left col-3">
-            <nav className="nav flex-column my-5">
-              <div className="nav-element">
-                <NavLink className="nav-link text-white fw-bold" to="/lostandfound/lost">
-                  <FontAwesomeIcon icon={faSearch} /> &nbsp; Lost
-                </NavLink>
-              </div>
-              <div className="nav-element">
-                <NavLink className="nav-link text-white fw-bold" to="/lostandfound/found">
-                  <FontAwesomeIcon icon={faPaw} /> &nbsp; Found
-                </NavLink>
-              </div>
-            </nav>
-          </div>
-
-
-          <div className="lost-found-pets col-7">
-            {isLostMode ? <Lost /> : <Found />}
-          </div>
-
-          <div className="sidebar-right col-2"> </div>
+        <div className="lost-found-pets col-7">
+          {isLostMode ? <Lost /> : <Found />}
         </div>
       </section>
     </div>
--- src/components/LostAndFound/PetProfileLost.js
+++ src/components/LostAndFound/PetProfileLost.js
@@ -1,94 +1,107 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import {NavLink, useParams} from "react-router-dom";
-import {setLocalLostPet} from "../../store/actions/actionPets";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMapMarker, faPaw, faSearch} from "@fortawesome/free-solid-svg-icons";
+import { NavLink, useParams } from "react-router-dom";
+import { setLocalLostPet } from "../../store/actions/actionPets";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarker,
+  faPaw,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import Lost from "./Lost";
 import Found from "./Found";
 
 const PetProfileLost = ({ pet, setLostPet }) => {
-
   const { id } = useParams();
 
-    useEffect(() => {
-        setLostPet(+id)
-        }, []);
+  useEffect(() => {
+    setLostPet(+id);
+  }, []);
 
   return (
-
-      <div>
-          <header className="homepage-header px-4 py-2">
-              <div className="blue-logo blue-logo-header">
-                  <NavLink to="/"> </NavLink>
+    <div>
+      <section className="container-fluid">
+        <div className="pet-profile-wrapper col-7 p-5">
+          <div>
+            <h3>
+              {" "}
+              <span className="blue">Lost pet:</span> {pet.nick} |{" "}
+            </h3>
+            <p className="card-text">
+              <FontAwesomeIcon
+                style={{ color: "#06B2BB9A" }}
+                icon={faMapMarker}
+              />{" "}
+              {pet.location}
+            </p>
+          </div>
+          <br />
+          <div className="card mb-3" style={{ maxWidth: "540px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src={
+                    (pet.photo =
+                      "aaa.jpg" || "bbb.jpg" || " "
+                        ? "https://offvkontakte.ru/wp-content/uploads/avatarka-pustaya-vk_20.jpg"
+                        : pet.photo)
+                  }
+                  className="card-img-top p-2"
+                  alt="pet"
+                />
               </div>
-          </header>
-
-          <section className="container-fluid">
-              <div className="row align-items-stretch ">
-                  <div className="sidebar-left col-3">
-                      <nav className="nav flex-column my-5">
-                          <div className="nav-element">
-                              <NavLink className="nav-link text-white fw-bold" to="/lostandfound/lost">
-                                  <FontAwesomeIcon icon={faSearch} /> {" "} Lost
-                              </NavLink>
-                          </div>
-                          <div className="nav-element">
-                              <NavLink className="nav-link text-white fw-bold" to="/lostandfound/found">
-                                  <FontAwesomeIcon icon={faPaw} /> {" "} Found
-                              </NavLink>
-                          </div>
-                      </nav>
-                  </div>
-
-                  <div className="pet-profile-wrapper col-7 p-5">
-                      <div>
-                          <h3> <span className="blue">Lost pet:</span> {pet.nick} | </h3>
-                          <p className="card-text"><FontAwesomeIcon style={{color: "#06B2BB9A"}} icon={faMapMarker}/> {pet.location}</p>
-                      </div><br/>
-                      <div className="card mb-3" style={{maxWidth: "540px"}}>
-                          <div className="row g-0">
-                              <div className="col-md-4">
-                                  <img src={ pet.photo = "aaa.jpg"||"bbb.jpg"||" " ? "https://offvkontakte.ru/wp-content/uploads/avatarka-pustaya-vk_20.jpg" : pet.photo} className="card-img-top p-2" alt="pet"/>
-                              </div>
-                              <div className="col-md-8">
-                                  <div className="card-body">
-                                      <h3 className="card-title"><span className="blue">{pet.type}</span> - {pet.breed}</h3>
-                                      <p className="card-text"><small className="text-muted">{pet.createdAt}</small></p>
-                                      <hr/>
-                                      <p><span className="blue">Color:</span> {pet.color} </p>
-                                      <p><span className="blue">Sex:</span>  {pet.sex} </p>
-                                      <p><span className="blue">Height:</span> {pet.height} </p>
-                                      <p><span className="blue">Distinctive features: </span> {pet.disFeatures} </p>
-                                      <p><span className="blue">Description:</span> {pet.description} </p><br/>
-                                      <p><span className="blue"> Phone: </span> {pet.phone} </p>
-                                      <p><span className="blue">  E-Mail:  </span>  {pet.email} </p>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-
-
-
-                  <div className="sidebar-right col-2"> </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h3 className="card-title">
+                    <span className="blue">{pet.type}</span> - {pet.breed}
+                  </h3>
+                  <p className="card-text">
+                    <small className="text-muted">{pet.createdAt}</small>
+                  </p>
+                  <hr />
+                  <p>
+                    <span className="blue">Color:</span> {pet.color}{" "}
+                  </p>
+                  <p>
+                    <span className="blue">Sex:</span> {pet.sex}{" "}
+                  </p>
+                  <p>
+                    <span className="blue">Height:</span> {pet.height}{" "}
+                  </p>
+                  <p>
+                    <span className="blue">Distinctive features: </span>{" "}
+                    {pet.disFeatures}{" "}
+                  </p>
+                  <p>
+                    <span className="blue">Description:</span> {pet.description}{" "}
+                  </p>
+                  <br />
+                  <p>
+                    <span className="blue"> Phone: </span> {pet.phone}{" "}
+                  </p>
+                  <p>
+                    <span className="blue"> E-Mail: </span> {pet.email}{" "}
+                  </p>
+                </div>
               </div>
-          </section>
-      </div>
-
-  )
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
   return {
-      pet: state.pets.petById,
+    pet: state.pets.petById,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        setLostPet: (id) => dispatch(setLocalLostPet(id)),
-    };
+  return {
+    setLostPet: (id) => dispatch(setLocalLostPet(id)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PetProfileLost);

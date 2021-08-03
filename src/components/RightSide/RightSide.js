58097ae75cca6c09cd5f9919cf05ac101cee06a2
LisaPenzareva <penzarevaea@gmail.com>
2021-08-03 13:02:31 +0200

    userprofile

--- src/components/RightSide/RightSide.js
+++ src/components/RightSide/RightSide.js
@@ -1,15 +1,21 @@
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {setUserById} from "../../store/actions/actionUsers";
-import {connect} from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserById } from "../../store/actions/actionUsers";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Logout from "./Logout";
 
-const RightSide = ({user}) => {
-
+const RightSide = ({ user }) => {
   const isAuth = useSelector((state) => {
     return state.users.isAuth;
   });
 
+  const history = useHistory();
   const dispatch = useDispatch();
+  const showUserProfile = (event) => {
+    event.preventDefault();
+    history.push("/homepage/:localStorage.userId");
+  };
 
   useEffect(() => {
     dispatch(setUserById(+localStorage.userId));
@@ -22,25 +28,43 @@ const RightSide = ({user}) => {
   // console.log(user)
 
   const renderUserFullName = () => {
-    return(
-    <div>
-      <p>{user.fullName}</p>
-    </div> )}
+    return (
+      <div>
+        <a
+          className="text-white text-decoration-none"
+          href="#"
+          onClick={showUserProfile}
+        >
+          {user.fullName}
+        </a>
+      </div>
+    );
+  };
 
-  return <section className="sidebar-right col-2">
-
-    { !isAuth ?
-        null :
+  return (
+    <section className="sidebar-right col-2">
+      {!isAuth ? null : (
         <div>
-          <hr style={{color: "white"}}/>
+          <hr style={{ color: "white" }} />
           <div className="d-flex justify-content-between px-1">
-            <img src="https://okeygeek.ru/wp-content/uploads/2020/03/no_avatar.png" className="user-image" alt="avatar"/>
-            <div className="w-50 text-white d-flex align-items-center" style={{fontSize: "16px"}}>{renderUserFullName()}</div>
+            <img
+              src="https://okeygeek.ru/wp-content/uploads/2020/03/no_avatar.png"
+              className="user-image"
+              alt="avatar"
+            />
+            <div
+              className="w-50 text-white d-flex align-items-center"
+              style={{ fontSize: "16px" }}
+            >
+              {renderUserFullName()}
+            </div>
           </div>
-          <hr style={{color: "white"}}/>
+          <hr style={{ color: "white" }} />
+          <Logout />
         </div>
-    }
-  </section>;
+      )}
+    </section>
+  );
 };
 
 const mapStateToProps = (state) => {
--- src/store/actions/actionUsers.js
+++ src/store/actions/actionUsers.js
@@ -1,86 +1,105 @@
-import {ADD_NEW_USER, CHANGE_AUTH, SET_USER_BY_ID} from "../typesList";
+import { ADD_NEW_USER, CHANGE_AUTH, SET_USER_BY_ID } from "../typesList";
 
 export const addNewUser = (data) => {
-    return async (dispatch) => {
-        try {
-            const response = await fetch("http://propets.telran-edu.de:8080/api/v1/auth/signup", {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            });
-            const json = await response.json();
-            await dispatch(addUser(json));
-        } catch (err) {
-            console.log(err.message);
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "http://propets.telran-edu.de:8080/api/v1/auth/signup",
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
         }
-    };
+      );
+      const json = await response.json();
+      await dispatch(addUser(json));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 };
 
 const addUser = (user) => {
-    return {
-        type: ADD_NEW_USER,
-        payload: user,
-    };
+  return {
+    type: ADD_NEW_USER,
+    payload: user,
+  };
 };
 
 export const doSignIn = (user) => {
-    return async (dispatch) => {
-        try {
-            const response = await fetch("http://propets.telran-edu.de:8080/api/v1/auth/signin", {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(user)
-            });
-            const json = await response.json();
-            await localStorage.setItem('token', json.accessToken)
-            await localStorage.setItem('userId', json.id)
-            console.log(json)
-
-            await dispatch(changeAuth(true))
-        } catch (err) {
-            console.log(err.message);
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "http://propets.telran-edu.de:8080/api/v1/auth/signin",
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
         }
+      );
+      const json = await response.json();
+      await localStorage.setItem("token", json.accessToken);
+      await localStorage.setItem("userId", json.id);
+      console.log(json);
+
+      await dispatch(changeAuth(true));
+    } catch (err) {
+      console.log(err.message);
     }
-}
+  };
+};
 
 const changeAuth = (authMode) => {
-    return {
-        type: CHANGE_AUTH,
-        payload: authMode,
-    }
-}
-
+  return {
+    type: CHANGE_AUTH,
+    payload: authMode,
+  };
+};
 
 export const setUserById = (userId) => {
-    return async(dispatch) => {
-        try {
-            const response = await fetch(`http://propets.telran-edu.de:8080/api/v1/users/${userId}`, {
-                method: "GET",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-api-key": localStorage.token
-                },
-            });
-            const json = await response.json();
-            // console.log(json);
-            await dispatch(setUserByIdInState(json));
-        } catch (err) {
-            console.log(err.message);
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        `http://propets.telran-edu.de:8080/api/v1/users/${userId}`,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": localStorage.token,
+          },
         }
-    };
+      );
+      const json = await response.json();
+      // console.log(json);
+      await dispatch(setUserByIdInState(json));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 };
 
 const setUserByIdInState = (user) => {
-    return {
-        type: SET_USER_BY_ID,
-        payload: user,
-    };
+  return {
+    type: SET_USER_BY_ID,
+    payload: user,
+  };
 };
 
+export const logOut = () => {
+  return async (dispatch) => {
+    try {
+      await localStorage.removeItem("token");
+      await localStorage.removeItem("userId");
+      await dispatch(changeAuth(false));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+};
